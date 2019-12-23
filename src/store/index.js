7d4d70c100f6a09ef89738273d2059c44fda1060
zhang <1249177017@qq.com>
2019-12-23 16:12:46 +0800

    centerloginsetup

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-	  isprovince: true,
-	  provinceid: Number
-  },
+  state: { isprovince: true, provinceid: Number },
   mutations: {
   },
   actions: {
