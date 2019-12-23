07820f8bdf8f132ca50225374e502c1ff0122296
sun <897495837@qq.com>
2019-12-23 16:05:52 +0800

    category

--- src/store/index.js
+++ src/store/index.js
@@ -10,16 +10,18 @@ export default new Vuex.Store({
   },
   mutations: {
     GetCategoryListMutation (state, data) {
+      state.CategoryList = []
       state.CategoryList = data
     },
     GetSmallListMutation (state, data) {
+      state.CategorySmallList = []
       state.CategorySmallList = data
     }
   },
   actions: {
     getCategoryList (store) {
       Axios({
-        url: `https://mobileway.lecuntao.com/lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1577061675581&act=mobile_cate&op=index`,
+        url: `lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1577061675581&act=mobile_cate&op=index`,
         method: 'post'
       }).then(res => {
         store.commit('GetCategoryListMutation', res.data.datas)
@@ -27,8 +29,24 @@ export default new Vuex.Store({
     },
     getCategorySmallList (store, id) {
       Axios({
-        url: `https://mobileway.lecuntao.com/lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1577063742794&act=mobile_cate&op=index`,
-        method: 'post'
+        method: 'post',
+        url: `lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1577061675581&act=mobile_cate&op=index`,
+        data: {
+          gc_id: id
+        },
+        transformRequest: [
+          function (data) {
+            let ret = ''
+            for (let it in data) {
+              ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+            }
+            ret = ret.substring(0, ret.lastIndexOf('&'))
+            return ret
+          }
+        ],
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
       }).then(res => {
         store.commit('GetSmallListMutation', res.data.datas)
       })
--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,33 +1,99 @@
 <template>
-    <div>
-       <mt-navbar  v-model="selected">
-         <mt-tab-item v-for="name in this.$store.state.CategoryList" :key="name.gc_id" :id="name.gc_id" @click="getCategorySmallList(name.gc_id)">{{name.gc_name}}</mt-tab-item>
-       </mt-navbar>
-
-       <!-- tab-container -->
-       <mt-tab-container v-model="selected">
-         <mt-tab-container-item v-for="name in this.$store.state.CategoryList" :key="name.gc_id" :id="name.gc_id">
-           <mt-cell v-for="n in this.$store.state.CategorySmallList" :title="n.gc_name" :key="n.gc_id" />
-         </mt-tab-container-item>
-       </mt-tab-container>
+  <div class="Category">
+    <ul>
+      <li class="" v-for="name in $store.state.CategoryList" :key="name.gc_id" :id="name.gc_id" @click="getCategorySmallList(name.gc_id);onclick($event)">{{ name.gc_name }}</li>
+    </ul>
+    <div id="Smalllist">
+      <div v-for="n in $store.state.CategorySmallList" :key="n.gc_id" :id="n.gc_id">
+        <img :src="n.cate_img" />
+        <span>{{ n.gc_name }}</span>
+      </div>
     </div>
+  </div>
+
 </template>
 
 <script>
+// console.log(this)
 export default {
-
   data () {
-    return {
-      selected: null
-    }
+    return {}
   },
   methods: {
     getCategorySmallList (id) {
+      //   if (localStorage.getItem('CategorySmallList')) {
+      //     ;
+      //   } else {
       this.$store.dispatch('getCategorySmallList', id)
+      // localStorage.setItem('CategorySmallList', this.$store.state.CategorySmallList)
+      //   }
+    },
+    onclick (e) {
+      let list = document.querySelectorAll('.Category li')
+      for (let a = 0; a < list.length; a++) {
+        if (list[a].className === 'onclickList') {
+          list[a].classList.remove('onclickList')
+        }
+      }
+      e.target.classList.add('onclickList')
     }
   },
   beforeCreate () {
+    // if (localStorage.getItem('CategoryList')) {
+    //   ;
+    // } else {
     this.$store.dispatch('getCategoryList')
+    // localStorage.setItem('CategoryList', this.$store.state.CategoryList)
+    // }
+  },
+  mounted () {
+    let a = document.querySelector('.Category li:nth-of-type(1)')
+    a.classList.add('onclickList')
+    this.$store.dispatch('getCategorySmallList', a.getAttribute('key'))
   }
 }
 </script>
+
+<style>
+.Category {
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+.Category ul {
+  position: absolute;
+  left: 0;
+  width: 5rem;
+  height: 100%;
+}
+.Category li {
+  height: 3rem;
+  line-height: 3rem;
+  text-align: center;
+  background-color: rgb(236,236,236);
+}
+#Smalllist {
+  width: 17rem;
+  position: absolute;
+  right: 0;
+}
+#Smalllist div{
+  height: 5%;
+  width: 33%;
+  text-align: center;
+  float: left;
+}
+#Smalllist img{
+  width: 100%;
+}
+#Smalllist span{
+  display: inline-block;
+  height: 1rem;
+  line-height: 1rem;
+  width: 100%;
+}
+.onclickList{
+  background-color: white;
+  color: red;
+}
+</style>
