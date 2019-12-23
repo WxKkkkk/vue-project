59cf335061034f506eea8f002904379544ef0b80
sun <897495837@qq.com>
2019-12-23 10:06:36 +0800

    category

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import Axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    CategoryList: [],
+    CategorySmallList: []
   },
   mutations: {
+    GetCategoryListMutation (state, data) {
+      state.CategoryList = data
+    },
+    GetSmallListMutation (state, data) {
+      state.CategorySmallList = data
+    }
   },
   actions: {
+    getCategoryList (store) {
+      Axios({
+        url: `https://mobileway.lecuntao.com/lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1577061675581&act=mobile_cate&op=index`,
+        method: 'post'
+      }).then(res => {
+        store.commit('GetCategoryListMutation', res.data.datas)
+      })
+    },
+    getCategorySmallList (store, id) {
+      Axios({
+        url: `https://mobileway.lecuntao.com/lct?api_version=2.3.0&platType=2&client=wap&isEncry=0&time=1577063742794&act=mobile_cate&op=index`,
+        method: 'post'
+      }).then(res => {
+        store.commit('GetSmallListMutation', res.data.datas)
+      })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,5 +1,33 @@
 <template>
     <div>
-        Category
+       <mt-navbar  v-model="selected">
+         <mt-tab-item v-for="name in this.$store.state.CategoryList" :key="name.gc_id" :id="name.gc_id" @click="getCategorySmallList(name.gc_id)">{{name.gc_name}}</mt-tab-item>
+       </mt-navbar>
+
+       <!-- tab-container -->
+       <mt-tab-container v-model="selected">
+         <mt-tab-container-item v-for="name in this.$store.state.CategoryList" :key="name.gc_id" :id="name.gc_id">
+           <mt-cell v-for="n in this.$store.state.CategorySmallList" :title="n.gc_name" :key="n.gc_id" />
+         </mt-tab-container-item>
+       </mt-tab-container>
     </div>
 </template>
+
+<script>
+export default {
+
+  data () {
+    return {
+      selected: null
+    }
+  },
+  methods: {
+    getCategorySmallList (id) {
+      this.$store.dispatch('getCategorySmallList', id)
+    }
+  },
+  beforeCreate () {
+    this.$store.dispatch('getCategoryList')
+  }
+}
+</script>
