a42b7dce63b48cbf8737e325604052660900e389
wxk <2220051599@qq.com>
2019-12-24 18:51:53 +0800

    第二天详情页

--- src/main.js
+++ src/main.js
@@ -2,18 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-<<<<<<< HEAD
+
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
 import ElementUi from 'element-ui'
 import '../node_modules/element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUi)
-
-=======
-import MintUI from 'mint-ui'
-import 'mint-ui/lib/style.css'
->>>>>>> b0271641196df457c481168be80c14e212bc695e
 Vue.use(MintUI)
 
 Vue.config.productionTip = false
